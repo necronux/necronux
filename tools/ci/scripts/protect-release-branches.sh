1fa07847e7a81e4e4b2b723c2ca26317980afdc3
NayanTheSpaceGuy <147062173+NayanTheSpaceGuy@users.noreply.github.com>
2025-03-07 17:30:35 +0530

    add protect release branch job and script and some minor changes

--- tools/ci/scripts/protect-release-branches.sh
+++ tools/ci/scripts/protect-release-branches.sh
@@ -6,30 +6,31 @@
 # SPDX-License-Identifier: Apache-2.0
 # ==-----------------------------------------------------------== #
 
-# Define target branch
-TARGET_BRANCH="develop"
+# Define target branch pattern
+TARGET_PATTERN="^release/v[0-9]+\.[0-9]+\.x$"
 
-# Define allowed branch prefixes as an array
-ALLOWED_PREFIXES=("bump/" "feature/" "bugfix/" "release-sync/" "dependabot/")
+# Define allowed source branch prefixes
+ALLOWED_PREFIXES=("bump/" "feature/" "bugfix/" "hotfix/")
 
 # Convert array to regex pattern
 ALLOWED_PATTERN="^($(IFS='|'; echo "${ALLOWED_PREFIXES[*]}"))"
 
-# Check if the pull request is targeting the develop branch
-if [[ "$GITHUB_BASE_REF" == "$TARGET_BRANCH" ]]; then
+# Check if the pull request is targeting the release branch
+if [[ "$GITHUB_BASE_REF" =~ $TARGET_PATTERN ]]; then
     # Check if the source branch follows the allowed naming pattern
     if ! [[ "$GITHUB_HEAD_REF" =~ $ALLOWED_PATTERN ]]; then
-        printf "Error: Pull requests to '%s' can only be from these branches:\n" "$GITHUB_BASE_REF"
+        printf "Error: Pull requests to '%s' branches can only be from these branches:\n" "$GITHUB_BASE_REF"
         for branch in "${ALLOWED_PREFIXES[@]}"; do
             printf "   - %s\n" "$branch"
         done
         exit 1
     else
-        echo "Pull request to '$TARGET_BRANCH' from allowed branch '$GITHUB_HEAD_REF'."
+        echo "Pull request to '$GITHUB_BASE_REF' from allowed branch '$GITHUB_HEAD_REF'."
     fi
 else
-    # If not targeting 'develop', skip the check
-    echo "Pull request is not targeting '$TARGET_BRANCH'; skipping the check."
+    # If not targeting a release branch, skip the check
+    echo "Pull request is not targeting a release branch; skipping the check."
 fi
 
 exit 0
+
