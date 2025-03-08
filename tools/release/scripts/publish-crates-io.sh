db712d1aee85dc6d65c4e8ec03b7c074f5cf7a03
NayanTheSpaceGuy <147062173+NayanTheSpaceGuy@users.noreply.github.com>
2025-03-08 10:47:42 +0530

    update publish crates io script to use cargo publish in loop

--- tools/release/scripts/publish-crates-io.sh
+++ tools/release/scripts/publish-crates-io.sh
@@ -5,22 +5,39 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 # ==-----------------------------------------------------------== #
-#
-# Check if reuse is installed
+
+if [ -n "$(git status --porcelain)" ]; then
+    echo "You have local changes!"
+    exit 0
+fi
+
 if ! reuse --version &> /dev/null
 then
     echo "Error: reuse is not installed. Install it first."
     exit 1
 fi
 
-# Define the output file for the SBOM (optional)
 OUTPUT_FILE="docs/reuse.spdx"
 
-# Run reuse sbom and save to the output file
 if reuse spdx --output "$OUTPUT_FILE"; then
     echo "SBOM generated successfully: $OUTPUT_FILE"
-    exit 0
 else
     echo "Error: Failed to generate SBOM"
     exit 1
 fi
+
+pushd crates || exit
+
+for crate in `cargo package --workspace 2>&1 | grep Packaging | sed 's_.*crates/\(.*\))_\1_' | grep -v Packaging`
+do
+  echo "Publishing ${crate}"
+  pushd "$crate" || exit
+  cargo publish
+  popd || exit
+  sleep 20
+done
+
+popd || exit
+
+echo "Publishing root crate"
+cargo publish
