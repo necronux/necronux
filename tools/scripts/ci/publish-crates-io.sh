cc778d841127baf92921fb44b837be9abfb07e56
NayanTheSpaceGuy <147062173+NayanTheSpaceGuy@users.noreply.github.com>
2025-03-30 01:45:48 +0530

    add dryrun crates.io job and updated script for ci use

--- tools/scripts/ci/publish-crates-io.sh
+++ tools/scripts/ci/publish-crates-io.sh
@@ -6,18 +6,13 @@
 # SPDX-License-Identifier: Apache-2.0
 # ==-----------------------------------------------------------== #
 
-if [ -n "$(git status --porcelain)" ]; then
-    echo "Error: You have uncommitted changes. Please commit or stash them before continuing."
-    exit 1
-fi
-
 if ! command -v reuse >/dev/null 2>&1; then
     echo "Error: 'reuse' is not installed. Please install it first."
     echo "More info: https://github.com/fsfe/reuse-tool"
     exit 1
 fi
 
-OUTPUT_FILE="docs/reuse.spdx"
+OUTPUT_FILE="reuse.spdx"
 echo "Generating SBOM at: $OUTPUT_FILE..."
 
 if reuse spdx --output "$OUTPUT_FILE"; then
@@ -30,15 +25,24 @@ fi
 pushd crates || { echo "Error: Failed to navigate to crates directory."; exit 1; }
 
 for crate in $(cargo package --workspace 2>&1 | grep Packaging | sed 's_.*crates/\(.*\))_\1_' | grep -v Packaging); do
-    echo "Publishing crate: $crate..."
-
     pushd "$crate" || { echo "Error: Failed to navigate into $crate"; exit 1; }
 
-    if cargo publish --allow-dirty; then
-        echo "Successfully published: $crate"
+    if [[ "$1" == "--dry-run" ]]; then
+        echo "Performing dry run for: $crate"
+        if cargo publish --locked --allow-dirty --dry-run; then
+            echo "Successfully performed dry run for: $crate"
+        else
+            echo "Error: Dry run failed for: $crate"
+            exit 1
+        fi
     else
-        echo "Error: Failed to publish: $crate"
-        exit 1
+        echo "Publishing crate: $crate"
+        if cargo publish --locked --allow-dirty; then
+            echo "Successfully published: $crate"
+        else
+            echo "Error: Failed to publish: $crate"
+            exit 1
+        fi
     fi
 
     popd || { echo "Error: Failed to return from $crate"; exit 1; }
@@ -50,9 +54,20 @@ done
 popd || { echo "Error: Failed to return from crates directory."; exit 1; }
 
 echo "Publishing the root crate..."
-if cargo publish --allow-dirty; then
-    echo "Successfully published the root crate."
+if [[ "$1" == "--dry-run" ]]; then
+    echo "Performing dry run for root crate"
+    if cargo publish --locked --allow-dirty --dry-run; then
+        echo "Successfully performed dry run for root crate."
+    else
+        echo "Error: Dry run failed for root crate."
+        exit 1
+    fi
 else
-    echo "Error: Failed to publish the root crate."
-    exit 1
+    echo "Publishing root crate..."
+    if cargo publish --locked --allow-dirty; then
+        echo "Successfully published the root crate."
+    else
+        echo "Error: Failed to publish the root crate."
+        exit 1
+    fi
 fi
